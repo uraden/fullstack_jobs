56e79c91427dd6be647a0faf22c83ec3e9eb3f74
uraden <jfahriddinov97@gmail.com>
2023-08-29 00:01:09 +0500

    jobs

--- be_jobs/app.js
+++ be_jobs/app.js
@@ -3,6 +3,15 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+
+// connect to DB
+
+const connectDB = require('./db/connect');
+
+// routes
+const authRouter = require('./routes/auth');
+const jobsRouter = require('./routes/jobs');
+
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
@@ -11,9 +20,8 @@ app.use(express.json());
 // extra packages
 
 // routes
-app.get('/', (req, res) => {
-  res.send('jobs api');
-});
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/jobs', jobsRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
@@ -22,9 +30,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
   }
