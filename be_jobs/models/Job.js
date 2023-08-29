83dfc9e8b44d12e5affc07b35235fb85e6606630
uraden <jfahriddinov97@gmail.com>
2023-08-30 00:23:27 +0500

    added new requests

--- be_jobs/models/Job.js
+++ be_jobs/models/Job.js
@@ -1,26 +1,29 @@
- const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
- const JobSchema = new mongoose.Schema({
+const JobSchema = new mongoose.Schema(
+  {
     company: {
-        type: String,
-        required: [true, 'Please provide company name'],
-        maxlength: 50
+      type: String,
+      required: [true, 'Please provide company name'],
+      maxlength: 50,
     },
     position: {
-        type: String,
-        required: [true, 'Please provide position'],
-        maxlength: 100
+      type: String,
+      required: [true, 'Please provide position'],
+      maxlength: 100,
     },
     status: {
-        type: String,
-        enum: ['interview', 'declined', 'pending'],
-        default: 'pending'
+      type: String,
+      enum: ['interview', 'declined', 'pending'],
+      default: 'pending',
     },
     createdBy: {
-        type: mongoose.Types.ObjectId,
-        ref: 'User',
-        required: [true, 'Please provide user']
-    }
-})
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: [true, 'Please provide user'],
+    },
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Job', JobSchema)
